7ecc980714a5094705b4c3aac64d8189fc6e24d5
Vickram <vickrammark54@gmail.com>
2025-01-02 10:46:11 +0530

    intial commit

--- Enemy Movement Pattern/index.js
+++ Enemy Movement Pattern/index.js
@@ -4,10 +4,12 @@ const ctx = canvas.getContext("2d");
 
 const CANVAS_WIDTH = (canvas.width = document.documentElement.clientWidth);
 const CANVAS_HEIGHT = (canvas.height = document.documentElement.clientHeight);
-const nubmerOfEnemies = 50;
+const nubmerOfEnemies = 10;
 let idVal = 1;
 let frameSpeed = 0;
 const enemiesArray = [];
+const enemies2Array = [];
+
 class Enemy {
   constructor() {
     this.image = new Image();
@@ -55,20 +57,63 @@ class Enemy {
       this.width,
       this.height
     );
+    // ctx.strokeRect(this.x, this.y, this.width, this.height);
+  }
+}
+class Enemy2 {
+  constructor() {
+    this.image = new Image();
+    this.image.src = "./enemies/enemy2.png";
+    this.spriteWidth = 266;
+    this.spriteHeight = 188;
+    this.id = idVal;
+    this.width = this.spriteWidth / 3;
+    this.height = this.spriteHeight / 3;
+    this.x = Math.random() * (CANVAS_WIDTH - this.width);
+    this.y = Math.random() * (CANVAS_HEIGHT - this.height);
+    this.speed = Math.floor(Math.random() * [4 - 1] + 1);
+    this.frames = 0;
+    this.previousFrame = 0;
+    this.flapSpeed = (this.speed - 1) / (4 - 1);
+    idVal = idVal + 1;
+  }
+  update() {
+    this.frames = this.frames < 5 ? ++this.frames : 0;
+    this.x -= this.speed;
+    this.previousFrame = this.frames;
+    //   this.y += Math.random() * 5 - 2.5;
+    if (this.x + this.width < 0) {
+      this.x = canvas.width;
+    }
+  }
+  draw() {
+    ctx.drawImage(
+      this.image,
+      this.frames * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+    // ctx.strokeRect(this.x, this.y, this.width, this.height);
   }
 }
 
 for (let i = 0; i < nubmerOfEnemies; i++) {
   enemiesArray.push(new Enemy());
+  enemies2Array.push(new Enemy2());
 }
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  enemiesArray.forEach((enemy, index) => {
+  enemies2Array.forEach((enemy, index) => {
     enemy.update();
     enemy.draw();
   });
-  frameSpeed = frameSpeed + 1;
+  frameSpeed = Math.random() * 1;
   requestAnimationFrame(animate);
 }
 
